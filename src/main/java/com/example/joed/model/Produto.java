9d9c2f9b2d38e78a94ad59b3ed80b685f4c51162
Eduardo <rm98890@fiap.com.br>
2024-04-16 15:44:10 -0300

    incluindo imports

--- src/main/java/com/example/joed/model/Produto.java
+++ src/main/java/com/example/joed/model/Produto.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
